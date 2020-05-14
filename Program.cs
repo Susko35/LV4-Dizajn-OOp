12d3e9b4e36da370e732879854cf04498a5e5d74
Antonio Šušnjara <64197201+Susko35@users.noreply.github.com>
2020-05-14 13:05:29 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LV4_Dizajn_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
