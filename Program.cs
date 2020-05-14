f588497250131c93268bb07e0fa2a96383e2744c
Antonio Šušnjara <64197201+Susko35@users.noreply.github.com>
2020-05-14 15:44:31 +0200

    Nije gotovo!

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace LV4_Dizajn_OOP
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Dataset csvFile = new Dataset("adapterinput.txt");
+            Analyzer3rdParty analyzer = new Analyzer3rdParty();
+            Adapter adapter = new Adapter(analyzer);
+            Console.WriteLine("Prosjek redaka: ");
+            for (int i = 0; i<3;i++)
+                Console.WriteLine(adapter.CalculateAveragePerRow(csvFile)[i]);
+            Console.WriteLine("Prosjek stupaca: ");
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(adapter.CalculateAveragePerColumn(csvFile)[i]);
+
         }
     }
 }
